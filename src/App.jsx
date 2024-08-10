abffdebb789a9b59df78f3ba58600d272b3a47c8
MoizSethi <m.moizsethi@gmail.com>
2024-08-11 00:28:00 +0500

    Use of Hooks

--- src/App.jsx
+++ src/App.jsx
@@ -33,7 +33,7 @@ function App() {
     <>
       <div className="w-full max-w-md mx-auto shadow-md rounded-lg px-4 py-3 my-8 bg-gray-700 text-orange-500">
         <h1 className="text-white text-center font-bold">Password Generator</h1>
-        <div className="flex-col shadow rounded-lg overflow-hidden mb-4">
+        <div className="flex shadow rounded-lg overflow-hidden mb-4">
           <input 
           type="text"
           value={password}
